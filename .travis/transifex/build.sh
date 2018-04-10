5a3876dbc9e0df3d9fb7e5f021b9e0969751ae48
liushuyu <liushuyu011@gmail.com>
2018-04-10 17:49:42 -0600

    travis: suggestions from @jroweboy

--- .travis/transifex/build.sh
+++ .travis/transifex/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -e
 
-echo "+docker run -e TRANSIFEX_API_TOKEN=[REDACTED] -v $(pwd):/yuzu alpine /bin/sh -e /yuzu/.travis/transifex/docker.sh"
 docker run -e TRANSIFEX_API_TOKEN="${TRANSIFEX_API_TOKEN}" -v "$(pwd)":/yuzu alpine /bin/sh -e /yuzu/.travis/transifex/docker.sh
