weihuoya/citra

travis: fix Transifex upload

.travis/transifex/build.sh
@@ -1,6 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-mkdir build && cd build
-cmake .. -DENABLE_QT_TRANSLATION=ON -DGENERATE_QT_TRANSLATION=ON -DCMAKE_BUILD_TYPE=Release
-make translation
-
+echo "+docker run -e TRANSIFEX_API_TOKEN=[REDACTED] -v $(pwd):/citra alpine /bin/sh -e /citra/.travis/transifex/docker.sh"
+docker run -e TRANSIFEX_API_TOKEN="${TRANSIFEX_API_TOKEN}" -v "$(pwd)":/citra alpine /bin/sh -e /citra/.travis/transifex/docker.sh

.travis/transifex/docker.sh
@@ -1,0 +1,26 @@
+#!/bin/bash -e
+
+# Setup RC file for tx
+echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
+
+set -x
+
+echo -e "\e[1m\e[33mInstalling dependencies...\e[0m"
+apk update
+apk add build-base sdl2-dev cmake python3-dev qt5-qttools-dev
+
+pip3 install transifex-client
+
+echo -e "\e[1m\e[33mBuild tools information:\e[0m"
+cmake --version
+gcc -v
+tx --version
+
+cd /citra
+mkdir build && cd build
+cmake .. -DENABLE_QT_TRANSLATION=ON -DGENERATE_QT_TRANSLATION=ON -DCMAKE_BUILD_TYPE=Release
+make translation
+cd ..
+
+cd dist/languages
+tx push -s

FearlessTobi/yuzu

.travis/transifex/build.sh
@@ -1,6 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-mkdir build && cd build
-cmake .. -DENABLE_QT_TRANSLATION=ON -DGENERATE_QT_TRANSLATION=ON -DCMAKE_BUILD_TYPE=Release
-make translation
-
+echo "+docker run -e TRANSIFEX_API_TOKEN=[REDACTED] -v $(pwd):/yuzu alpine /bin/sh -e /yuzu/.travis/transifex/docker.sh"
+docker run -e TRANSIFEX_API_TOKEN="${TRANSIFEX_API_TOKEN}" -v "$(pwd)":/yuzu alpine /bin/sh -e /yuzu/.travis/transifex/docker.sh

.travis/transifex/docker.sh
@@ -1,0 +1,26 @@
+#!/bin/bash -e
+
+# Setup RC file for tx
+echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
+
+set -x
+
+echo -e "\e[1m\e[33mInstalling dependencies...\e[0m"
+apk update
+apk add build-base sdl2-dev cmake python3-dev qt5-qttools-dev
+
+pip3 install transifex-client
+
+echo -e "\e[1m\e[33mBuild tools information:\e[0m"
+cmake --version
+gcc -v
+tx --version
+
+cd /yuzu
+mkdir build && cd build
+cmake .. -DENABLE_QT_TRANSLATION=ON -DGENERATE_QT_TRANSLATION=ON -DCMAKE_BUILD_TYPE=Release
+make translation
+cd ..
+
+cd dist/languages
+tx push -s