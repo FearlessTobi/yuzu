e6ab83ee663289cc620044d2d5b398800519b3a1
FearlessTobi <thm.frey@gmail.com>
2019-09-26 22:13:44 +0200

    Save progress

---
+++ src/core/hle/service/ldn/lan_discovery.h
@@ -0,0 +1,189 @@
+#pragma once
+#include <array>
+#include <cstring>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <unordered_map>
+#include <stdint.h>
+#include "core/hle/kernel/errors.h"
+#include "core/hle/service/ldn/lan_protocol.h"
+#include "core/hle/service/ldn/ldn_types.h"
+
+enum class NodeStatus : u8 {
+    Disconnected,
+    Connect,
+    Connected,
+};
+
+class LANDiscovery;
+
+class LanStation : public Pollable {
+protected:
+    friend class LANDiscovery;
+    NodeInfo* nodeInfo;
+    NodeStatus status;
+    std::unique_ptr<TcpLanSocketBase> socket;
+    int nodeId;
+    LANDiscovery* discovery;
+
+public:
+    LanStation(int nodeId, LANDiscovery* discovery)
+        : nodeInfo(nullptr), status(NodeStatus::Disconnected), nodeId(nodeId),
+          discovery(discovery) {}
+    NodeStatus getStatus() const {
+        return this->status;
+    }
+    void reset() {
+        this->socket.reset();
+        this->status = NodeStatus::Disconnected;
+    };
+    void link(int fd) {
+        this->socket = std::make_unique<TcpLanSocketBase>(fd);
+        this->status = NodeStatus::Connect;
+    };
+    int getFd() override {
+        if (!this->socket) {
+            return -1;
+        }
+        return this->socket->getFd();
+    };
+    int onRead() override;
+    void onClose() override;
+    int sendPacket(LANPacketType type, const void* data, size_t size) {
+        if (!this->socket) {
+            return -1;
+        }
+        return this->socket->sendPacket(type, data, size);
+    };
+    void overrideInfo() {
+        bool connected = this->getStatus() == NodeStatus::Connected;
+        this->nodeInfo->nodeId = this->nodeId;
+        if (connected) {
+            this->nodeInfo->isConnected = 1;
+        } else {
+            this->nodeInfo->isConnected = 0;
+        }
+    }
+};
+
+class LDUdpSocket : public UdpLanSocketBase, public Pollable {
+protected:
+    struct MacHash {
+        std::size_t operator()(const MacAddress& t) const {
+            return *reinterpret_cast<const u32*>(t.raw + 2);
+        }
+    };
+    virtual u32 getBroadcast() override;
+    LANDiscovery* discovery;
+
+public:
+    std::unordered_map<MacAddress, NetworkInfo, MacHash> scanResults;
+
+public:
+    LDUdpSocket(int fd, LANDiscovery* discovery);
+    int getFd() override {
+        return UdpLanSocketBase::getFd();
+    }
+    int onRead() override;
+    void onClose() override {
+        LOG_CRITICAL(Service_LDN, "LDUdpSocket::onClose");
+    };
+};
+
+class LDTcpSocket : public TcpLanSocketBase, public Pollable {
+protected:
+    LANDiscovery* discovery;
+
+public:
+    LDTcpSocket(int fd, LANDiscovery* discovery) : TcpLanSocketBase(fd), discovery(discovery){};
+    int getFd() override {
+        return TcpLanSocketBase::getFd();
+    }
+    int onRead() override;
+    void onClose() override;
+};
+
+class LANDiscovery {
+public:
+    static const int DefaultPort = 11452;
+    static const char* FakeSsid;
+    typedef std::function<int(LANPacketType, const void*, size_t)> ReplyFunc;
+    typedef std::function<void()> LanEventFunc;
+    static const LanEventFunc EmptyFunc;
+
+protected:
+    friend class LDUdpSocket;
+    friend class LDTcpSocket;
+    friend class LanStation;
+    // 0: udp 1: tcp 2: client
+    std::mutex pollMutex;
+    std::unique_ptr<LDUdpSocket> udp;
+    std::unique_ptr<LDTcpSocket> tcp;
+    std::array<LanStation, StationCountMax> stations;
+    std::array<NodeLatestUpdate, NodeCountMax> nodeChanges;
+    std::array<u8, NodeCountMax> nodeLastStates;
+    static void Worker(void* args);
+    bool stop;
+    bool inited;
+    NetworkInfo networkInfo;
+    u16 listenPort;
+    // HosThread workerThread;
+    CommState state;
+    void worker();
+    int loopPoll();
+    void onSyncNetwork(NetworkInfo* info);
+    void onConnect(int new_fd);
+    void onDisconnectFromHost();
+    void onNetworkInfoChanged();
+
+    void updateNodes();
+    void resetStations();
+    int getFakeMac(MacAddress* mac);
+    int getNodeInfo(NodeInfo* node, const UserConfig* userConfig, u16 localCommunicationVersion);
+    LanEventFunc lanEvent;
+
+public:
+    int initialize(LanEventFunc lanEvent = EmptyFunc, bool listening = true);
+    int finalize();
+    int initNetworkInfo();
+    ResultCode scan(NetworkInfo* networkInfo, u16* count, ScanFilter filter);
+    int setAdvertiseData(const u8* data, uint16_t size);
+    int createNetwork(const SecurityConfig* securityConfig, const UserConfig* userConfig,
+                      const NetworkConfig* networkConfig);
+    int destroyNetwork();
+    int connect(NetworkInfo* networkInfo, UserConfig* userConfig, u16 localCommunicationVersion);
+    int disconnect();
+    int getNetworkInfo(NetworkInfo* pOutNetwork);
+    int getNetworkInfo(NetworkInfo* pOutNetwork, NodeLatestUpdate* pOutUpdates, int bufferCount);
+    ResultCode openAccessPoint();
+    ResultCode closeAccessPoint();
+    ResultCode openStation();
+    ResultCode closeStation();
+
+public:
+    LANDiscovery(u16 port = DefaultPort)
+        : stations({{{1, this}, {2, this}, {3, this}, {4, this}, {5, this}, {6, this}, {7, this}}}),
+          stop(false), inited(false), networkInfo({0}), listenPort(port), state(CommState::None) {
+        LOG_CRITICAL(Service_LDN, "LANDiscovery");
+    };
+    ~LANDiscovery();
+    u16 getListenPort() const {
+        return this->listenPort;
+    }
+    CommState getState() const {
+        return this->state;
+    };
+    void setState(CommState v) {
+        this->state = v;
+        this->lanEvent();
+    };
+    int stationCount();
+
+protected:
+    int setSocketOpts(int fd);
+    int initTcp(bool listening);
+    int initUdp(bool listening);
+    void initNodeStateChange();
+    bool isNodeStateChanged();
+};
--- src/core/hle/service/ldn/ldn.cpp
+++ src/core/hle/service/ldn/ldn.cpp
@@ -4,9 +4,14 @@
 
 #include <memory>
 
+#include "core/core.h"
 #include "core/hle/ipc_helpers.h"
+#include "core/hle/kernel/readable_event.h"
+#include "core/hle/kernel/writable_event.h"
 #include "core/hle/result.h"
+#include "core/hle/service/ldn/lan_discovery.h"
 #include "core/hle/service/ldn/ldn.h"
+#include "core/hle/service/ldn/ldn_types.h"
 #include "core/hle/service/sm/sm.h"
 
 namespace Service::LDN {
@@ -102,15 +107,15 @@ public:
     explicit IUserLocalCommunicationService() : ServiceFramework{"IUserLocalCommunicationService"} {
         // clang-format off
         static const FunctionInfo functions[] = {
-            {0, nullptr, "GetState"},
+            {0, &IUserLocalCommunicationService::GetState, "GetState"},
             {1, nullptr, "GetNetworkInfo"},
             {2, nullptr, "GetIpv4Address"},
-            {3, nullptr, "GetDisconnectReason"},
+            {3,  &IUserLocalCommunicationService::GetDisconnectReason, "GetDisconnectReason"},
             {4, nullptr, "GetSecurityParameter"},
             {5, nullptr, "GetNetworkConfig"},
-            {100, nullptr, "AttachStateChangeEvent"},
+            {100, &IUserLocalCommunicationService::AttachStateChangeEvent, "AttachStateChangeEvent"},
             {101, nullptr, "GetNetworkInfoLatestUpdate"},
-            {102, nullptr, "Scan"},
+            {102, &IUserLocalCommunicationService::Scan, "Scan"},
             {103, nullptr, "ScanPrivate"},
             {104, nullptr, "SetWirelessControllerRestriction"},
             {200, nullptr, "OpenAccessPoint"},
@@ -123,13 +128,13 @@ public:
             {207, nullptr, "SetStationAcceptPolicy"},
             {208, nullptr, "AddAcceptFilterEntry"},
             {209, nullptr, "ClearAcceptFilter"},
-            {300, nullptr, "OpenStation"},
-            {301, nullptr, "CloseStation"},
+            {300, &IUserLocalCommunicationService::OpenStation, "OpenStation"},
+            {301, &IUserLocalCommunicationService::CloseStation, "CloseStation"},
             {302, nullptr, "Connect"},
             {303, nullptr, "ConnectPrivate"},
             {304, nullptr, "Disconnect"},
-            {400, nullptr, "Initialize"},
-            {401, nullptr, "Finalize"},
+            {400, &IUserLocalCommunicationService::Initialize, "Initialize"},
+            {401, &IUserLocalCommunicationService::Finalize, "Finalize"},
             {402, &IUserLocalCommunicationService::Initialize2, "Initialize2"}, // 7.0.0+
         };
         // clang-format on
@@ -144,6 +149,100 @@ public:
         IPC::ResponseBuilder rb{ctx, 2};
         rb.Push(RESULT_UNKNOWN);
     }
+
+    void onEventFired() {
+        LOG_CRITICAL(Service_LDN, "onEventFired signal_event");
+        state_event.writable->Signal();
+    }
+
+    void Initialize(Kernel::HLERequestContext& ctx) {
+        int result_code = lanDiscovery.initialize([&]() { this->onEventFired(); });
+
+        LOG_CRITICAL(Service_LDN, "called");
+
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(result_code);
+    }
+
+    void GetState(Kernel::HLERequestContext& ctx) {
+        LOG_CRITICAL(Service_LDN, "called");
+
+        IPC::ResponseBuilder rb{ctx, 3};
+        rb.Push(RESULT_SUCCESS);
+        rb.Push<u32>(static_cast<u32>(this->lanDiscovery.getState()));
+    }
+
+    void AttachStateChangeEvent(Kernel::HLERequestContext& ctx) {
+        LOG_CRITICAL(Service_LDN, "called");
+
+        IPC::ResponseBuilder rb{ctx, 2, 1};
+        rb.Push(RESULT_SUCCESS);
+        rb.PushCopyObjects(state_event.readable);
+    }
+
+    void GetDisconnectReason(Kernel::HLERequestContext& ctx) {
+        LOG_CRITICAL(Service_LDN, "called");
+
+        IPC::ResponseBuilder rb{ctx, 3};
+        rb.Push(RESULT_SUCCESS);
+        rb.Push<u16>(0);
+    }
+
+    void OpenStation(Kernel::HLERequestContext& ctx) {
+        LOG_CRITICAL(Service_LDN, "called");
+
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(lanDiscovery.openStation());
+    }
+
+    void Scan(Kernel::HLERequestContext& ctx) {
+        // TODO: Check IPC shit if something breaks
+        LOG_CRITICAL(Service_LDN, "called");
+
+        IPC::RequestParser rp{ctx};
+        const auto channel{rp.Pop<u16>()};
+        const auto filter{rp.PopRaw<ScanFilter>()};
+
+        ResultCode rc = RESULT_SUCCESS;
+
+        NetworkInfo info{};
+        NetworkInfo* info_ptr = &info;
+
+        // TODO: Stubbed
+        u16 count = 0;
+        rc = lanDiscovery.scan(info_ptr, &count, filter);
+
+        std::array<u8, sizeof(info)> out_buf;
+        std::memcpy(&out_buf, &info, sizeof(info));
+
+        ctx.WriteBuffer(out_buf);
+
+        IPC::ResponseBuilder rb{ctx, 3};
+        rb.Push(rc);
+        rb.Push<u16>(count);
+    }
+
+    void Finalize(Kernel::HLERequestContext& ctx) {
+        LOG_CRITICAL(Service_LDN, "called");
+
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(lanDiscovery.finalize());
+    }
+
+    void CloseStation(Kernel::HLERequestContext& ctx) {
+        LOG_CRITICAL(Service_LDN, "called");
+
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(lanDiscovery.closeStation());
+    }
+
+    //TODO: Rocket League: 'SetSockOpt' when creating Lobby
+
+private:
+    LANDiscovery lanDiscovery;
+    Kernel::EventPair state_event = Kernel::WritableEvent::CreateEventPair(
+        Core::System::GetInstance().Kernel(), Kernel::ResetType::Automatic,
+        "IUserLocalCommunicationService:StateEvent");
 };
 
 class LDNS final : public ServiceFramework<LDNS> {
